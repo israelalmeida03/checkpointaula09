efcde899987ae8320ddd6aa70527171fb49866a5
israelalmeida03 <israelalmeida31@gmail.com>
2021-08-26 00:35:40 -0300

    Primeira Atualização

--- checkpoint.js
+++ checkpoint.js
@@ -11,80 +11,102 @@ Integrantes:
 4 - Feijão – 12 segundos (padrão);
 5 - Brigadeiro – 8 segundos (padrão);*/
 
-function microondas(opcao, tempo){
-    console.log("\nEste é o nosso menu:\n")
-    console.log("1 - Pipoca: 10 segundos")
-    console.log("2 - Macarrão: 8 segundos")
-    console.log("3 - Carne: 15 segundos")
-    console.log("4 - Feijão: 12 segundos")
-    console.log("5 - Brigadeiro: 8 segundos\n")
+function microondas(opcao, tempoSelecionado){
+    let opcaoSelecionada;
+    let tempoPadrao;
+    console.log("---------------------Menu---------------------\n")
+    
+    console.log("1 - Pipoca: 10 segundos(padrão) ")
+    console.log("2 - Macarrão: 8 segundos(padrão)")
+    console.log("3 - Carne: 15 segundos(padrão)")
+    console.log("4 - Feijão: 12 segundos(padrão)")
+    console.log("5 - Brigadeiro: 8 segundos(padrão)\n")
+    console.log("Selecione a opção e informe o tempo desejado:\n")
+
+    if(opcao == 1){
+        opcaoSelecionada = "Pipoca"
+        tempoPadrao = 10;
+    }else if(opcao == 2){
+        opcaoSelecionada = "Macarrão"
+        tempoPadrao = 8;
+    }else if(opcao == 3){
+        opcaoSelecionada = "Carne"
+        tempoPadrao = 15;
+    }else if(opcao == 4){
+        opcaoSelecionada = "Feijão"
+        tempoPadrao = 12;
+    }else{
+        opcaoSelecionada = "Brigadeiro"
+        tempoPadrao = 8;
+    }
    
     switch(opcao){
-        case 1: //Pipoca-10s
-            console.log("Pipoca, tempo selecionado: " +tempo+ " segundos");
-            if(opcao == 1 && tempo > 20){
+        case 1 : //Pipoca-10s
+            console.log("Opção selecionada: "+opcaoSelecionada +"\nTempo definido: " +tempoSelecionado+ " segundos");
+            if(tempoSelecionado>= (tempoPadrao * 2) && tempoSelecionado < (tempoPadrao * 3) ){
                 console.log("A comida queimou!!!")
-            }else if(tempo < 10){
+            }else if(tempoSelecionado < tempoPadrao){
                 console.log("Tempo insuficiente")
-            }else if(tempo > 30){
+            }else if(tempoSelecionado >= (tempoPadrao * 3)){
                 console.log("Kabuummmm!!!!!")
             }else{
                 console.log("Prato pronto, bom apetite")
             }
             break;
         case 2: //Macarrao-8s
-        console.log("Macarrão, tempo selecionado: " +tempo+ " segundos");
-            if(tempo > 16){
+        console.log("Opção selecionada: "+opcaoSelecionada +"\nTempo definido: " +tempoSelecionado+ " segundos");
+            if(tempoSelecionado>= (tempoPadrao * 2) && tempoSelecionado < (tempoPadrao * 3) ){
                 console.log("A comida queimou!!!")
-            }else if(tempo < 8){
+            }else if(tempoSelecionado < tempoPadrao){
                 console.log("Tempo insuficiente")
-            }else if(tempo > 24){
+            }else if(tempoSelecionado >= (tempoPadrao * 3)){
                 console.log("Kabuummmm!!!!!")
             }else{
                 console.log("Prato pronto, bom apetite")
             }
             break;
         case 3: //Carne-15s
-        console.log("Carne, tempo selecionado: " +tempo+ " segundos");
-            if(tempo > 30){
+        console.log("Opção selecionada: "+opcaoSelecionada +"\nTempo definido: " +tempoSelecionado+ " segundos");
+            if(tempoSelecionado>= (tempoPadrao * 2) && tempoSelecionado < (tempoPadrao * 3) ){
                 console.log("A comida queimou!!!")
-            }else if(tempo < 15){
+            }else if(tempoSelecionado < tempoPadrao){
                 console.log("Tempo insuficiente")
-            }else if(tempo > 45){
+            }else if(tempoSelecionado >= (tempoPadrao * 3)){
                 console.log("Kabuummmm!!!!!")
             }else{
                 console.log("Prato pronto, bom apetite")
             }
             break;
         case 4: //Feijao-12s
-        console.log("Feijão, tempo selecionado: " +tempo+ " segundos");
-            if(tempo > 24){
+        console.log("Opção selecionada: "+opcaoSelecionada +"\nTempo definido: " +tempoSelecionado+ " segundos");
+            if(tempoSelecionado>= (tempoPadrao * 2) && tempoSelecionado < (tempoPadrao * 3) ){
                 console.log("A comida queimou!!!")
-            }else if(tempo < 12){
+            }else if(tempoSelecionado < tempoPadrao){
                 console.log("Tempo insuficiente")
-            }else if(tempo > 36){
-                console.log("Kabuummmm!!!!!")
-                }else{
-                console.log("Prato pronto, bom apetite")
-            }
-            break;
-        case 5: //Brigadeiro-8s
-        console.log("Brigadeiro, tempo selecionado: " +tempo+ " segundos");
-            if(tempo > 16){
-                console.log("A comida queimou!!!")
-            }else if(tempo < 8){
-                console.log("Tempo insuficiente")
-            }else if(tempo > 24){
+            }else if(tempoSelecionado >= (tempoPadrao * 3)){
                 console.log("Kabuummmm!!!!!")
             }else{
                 console.log("Prato pronto, bom apetite")
             }
             break;
+        case 5: //Brigadeiro-8s
+        console.log("Opção selecionada: "+opcaoSelecionada +"\nTempo definido: " +tempoSelecionado+ " segundos");
+            if(tempoSelecionado>= (tempoPadrao * 2) && tempoSelecionado < (tempoPadrao * 3) ){
+                console.log("A comida queimou!!!")
+            }else if(tempoSelecionado < tempoPadrao){
+                console.log("Tempo insuficiente")
+            }else if(tempoSelecionado >= (tempoPadrao * 3)){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+
             default: //Nenhuma das opções
             console.log("Opção inválida!!!")
             break;
             }
             }
       
-        // teste pipoca 15s
-        microondas(5, 8)
+        
+        microondas(1, 24)
